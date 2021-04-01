a39b387d8cb21c19a585731413f24649f0cc12ee
Yamada <ymdtoko@yahoo.co.jp>
2021-04-01 09:50:46 +0000

    S3変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-password = "123456abC"
+password = ENV['LOGIN_PASSWORD']
 
 User.create(
     [

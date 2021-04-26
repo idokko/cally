103d5f3f08cbff37fa35782cf6d0a05a6764104d
Yamada <ymdtoko@yahoo.co.jp>
2021-04-26 14:23:53 +0000

    production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   #Mailerの設定 
   # SendGridを使用
   config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliver = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'boiling-sea-28983.herokuapp.com', port: $PORT, protocol: 'https' }

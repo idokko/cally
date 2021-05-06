bff3add757fc02afeb81f77fdf0b46c477d37e9e
Yamada <ymdtoko@yahoo.co.jp>
2021-05-06 14:09:47 +0000

    @message_count追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,28 +11,37 @@ class MessagesController < ApplicationController
             @room = @message.room
             @anotherRoomMember = Entry.where(room_id: @room.id).where.not(user_id: current_user.id)
             @theId = @anotherRoomMember.find_by(room_id: @room.id)
+            
             # ログインユーザーがメッセージを送ったとき
-            # binding.pry
-            unless @message.content.blank? && @message.images.attached?
-                notification = current_user.active_notifications.new(
-                    room_id: @room.id,
-                    message_id: @message.id,
-                    visited_id: @theId.user_id,
-                    visitor_id: current_user.id,
-                    checked: false,
-                    action: 'dm',
-                    content_checked: false,
-                    images_count: @message.images.size
-                )
-                # 自分のメッセージの場合は、通知済みとする
-                if notification.visitor_id == notification.visited_id
-                  notification.checked = true 
+            if @message.content.present? && @message.images.attached?
+                @message_count = @message.images.size + 1
+            elsif @message.images.attached?
+                @message_count = @message.images.size
+            else
+                @message_count = 1
+            end
+            
+            unless @message.content.blank? && @message.images.blank?
+            @message_count.times do
+                    notification = current_user.active_notifications.new(
+                        room_id: @room.id,
+                        message_id: @message.id,
+                        visited_id: @theId.user_id,
+                        visitor_id: current_user.id,
+                        checked: false,
+                        action: 'dm',
+                        content_checked: false,
+                    )
+                    # 自分のメッセージの場合は、通知済みとする
+                    if notification.visitor_id == notification.visited_id
+                      notification.checked = true 
+                    end
+                    # @message.contentの中身があるか確認
+                    unless @message.content.blank? || @message.images.blank?
+                        notification.content_checked = true
+                    end
+                    notification.save!
                 end
-                # @message.contentの中身があるか確認
-                unless @message.content.blank?
-                    notification.content_checked = true
-                end
-                notification.save!
             end
         redirect_to "/rooms/#{@message.room_id}"
         else
